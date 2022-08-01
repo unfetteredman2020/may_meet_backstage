78fe5a5ada8a7c74b98c5b9afd7198b820b61ccd
unfetteredman <weixw2014@qq.com>
2022-08-01 13:55:05 +0800

    add: 内容安全=>投诉管理

--- src/api/safeApi.js
+++ src/api/safeApi.js
@@ -1,3 +1,11 @@
 import request from './request.js'
+/**
+ * 
+ * 内容安全
+ */
 
+//投诉管理列表
 export const getReportManageList = data => request.get('/nraq/tsgl', { params: data })
+
+// 修改投诉状态
+export const changeReportHandle = data => request.post('/nraq/tsgl/xgzt', data)
